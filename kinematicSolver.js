3130af9a87dadd032846f832346b988761127464
Nolan Amblard <57413019+NolanAmblard@users.noreply.github.com>
2022-07-16 15:34:02 -0500

    Update kinematicSolver.js

--- kinematicSolver.js
+++ kinematicSolver.js
@@ -1,174 +1,174 @@
 function solveKinematics() {
 			
-			var x = document.getElementById("displace").value.toString().toLowerCase();
-			var v0 = document.getElementById("ivel").value.toString().toLowerCase();
-			var v = document.getElementById("fvel").value.toString().toLowerCase();
-			var a = document.getElementById("accel").value.toString().toLowerCase();
-			var t = document.getElementById("time").value.toString().toLowerCase();
-			
-			console.log("Inside of solveKinematics()");
-			
-			if( x=="x" && v0!="x" && v!="x" && a!="x" && t!="x") {
-                //Solve for only displacement
-                console.log("Inside of solve only for displacement if statement");
-                var dis = 0.0;
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                dis = vel0 * time + 0.5 * accel * (time * time);
-                return "dis: " + dis + "";
-            }
-            else if( x!="x" && v0=="x" && v!="x" && a!="x" && t!="x") {
-                //Solve for only initial velocity
-                var dis = parseFloat(x);
-                var vel0 = 0.0;
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                vel0 = vel - accel * time;
-                return "vel0: " + vel0 + "";
-           } 
-            else if( x!="x" && v0!="x" && v=="x" && a!="x" && t!="x") {
-                //Solve for only final velocity
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = 0.0;
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                vel = vel0 + accel * time;
-                return "velf: " + vel + "";
-            }
-            else if( x!="x" && v0!="x" && v!="x" && a=="x" && t!="x") {
-                //Solve for only acceleration
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = 0.0;
-                var time = parseFloat(t);
-                accel = (vel - vel0)/time;
-                return "accel: " + accel + "";
-            }
-            else if( x!="x" && v0!="x" && v!="x" && a!="x" && t=="x") {
-                //Solve for only time
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = 0.0;
-                time = (vel - vel0)/accel;
-                return "time: " + time + "";
-            }
-            else if( x=="x" && v0=="x" && v!="x" && a!="x" && t!="x") {
-                //Solve for displacement and initial velocity
-                var dis = 0.0;
-                var vel0 = 0.0;
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                vel0 = vel - accel * time;
-                dis = vel0 * time + 0.5 * accel * (time * time);
-                return "dis: " + dis + " vel0: " + vel0 + "";
-            }
-            else if( x=="x" && v0!="x" && v=="x" && a!="x" && t!="x") {
-                //Solve for displacement and final velocity
-                var dis = 0.0;
-                var vel0 = parseFloat(v0);
-                var vel = 0.0;
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                dis = vel0 * time + 0.5 * accel * (time * time);
-                vel = vel0 + accel * time;
-                return "dis: " + dis + " velf: " + vel + "";
-            }
-            else if( x=="x" && v0!="x" && v!="x" && a=="x" && t!="x") {
-                //Solve for displacement and acceleration
-                var dis = 0.0;
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = 0.0;
-                var time = parseFloat(t);
-                accel = (vel - vel0) / time;
-                dis = vel0 * time + 0.5 * accel * (time * time);
-                return "dis: " + dis + " accel: " + accel + "";
-            }
-            else if( x=="x" && v0!="x" && v!="x" && a!="x" && t=="x") {
-                //Solve for displacement and time
-                var dis = 0.0;
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = 0.0;
-                time = (vel - vel0) / accel;
-                dis = vel0 * time + 0.5 * accel * (time * time);
-                return "dis: " + dis + " time: " + time + "";
-            }
-            else if( x!="x" && v0=="x" && v=="x" && a!="x" && t!="x") {
-                //Solve for initial velocity and final velocity
-                var dis = parseFloat(x);
-                var vel0 = 0.0;
-                var vel = 0.0;
-                var accel = parseFloat(a);
-                var time = parseFloat(t);
-                vel0 = (dis - 0.5 * accel * (time * time))/time;
-                vel = vel0 + accel * time;
-                return "vel0: " + vel0 + " velf: " + vel + "";
-            }
-            else if( x!="x" && v0=="x" && v!="x" && a=="x" && t!="x") {
-                //Solve for initial velocity and acceleration
-                var dis = parseFloat(x);
-                var vel0 = 0.0;
-                var vel = parseFloat(v);
-                var accel = 0.0;
-                var time = parseFloat(t);
-                accel = (dis - vel * time) / (0.5 * (time * time) - (time * time));
-                vel0 = vel - accel * time;
-                return "vel0: " + vel0 + " accel: " + accel + "";
-            }
-            else if( x!="x" && v0=="x" && v!="x" && a!="x" && t=="x") {
-                //Solve for initial velocity and time
-                var dis = parseFloat(x);
-                var vel0 = 0.0;
-                var vel = parseFloat(v);
-                var accel = parseFloat(a);
-                var time = 0.0;
-                vel0 = Math.sqrt((vel * vel) - 2 * accel * dis);
-                time = (vel - vel0)/accel;
-                return "vel0: " + vel0 + " time: " + time + "";
-            }
-            else if( x!="x" && v0!="x" && v=="x" && a=="x" && t!="x") {
-                //Solve for final velocity and acceleration
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = 0.0;
-                var accel = 0.0;
-                var time = parseFloat(t);
-                accel = (dis - vel0 * time) / (2 * (time * time));
-                vel = vel0 + accel * time;
-                return "velf: " + vel + " accel: " + accel + "";
-            }
-            else if( x!="x" && v0!="x" && v=="x" && a!="x" && t=="x") {
-                //Solve for final velocity and time
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = 0.0;
-                var accel = parseFloat(a);
-                var time = 0.0;
-                vel = Math.sqrt((vel0 * vel0) + 2 * accel * dis);
-                time = (vel - vel0) / accel;
-                return "velf: " + vel + " time: " + time + "";
-            }
-            else if( x!="x" && v0!="x" && v!="x" && a=="x" && t=="x") {
-                //Solve for acceleration and time
-                var dis = parseFloat(x);
-                var vel0 = parseFloat(v0);
-                var vel = parseFloat(v);
-                var accel = 0.0;
-                var time = 0.0;
-                accel = ((vel * vel) - (vel0 * vel0)) / (2 * dis);
-                time = (vel - vel0) / accel;
-                return "accel: " + accel + " time: " + time + "";
-            }
+var x = document.getElementById("displace").value.toString().toLowerCase();
+var v0 = document.getElementById("ivel").value.toString().toLowerCase();
+var v = document.getElementById("fvel").value.toString().toLowerCase();
+var a = document.getElementById("accel").value.toString().toLowerCase();
+var t = document.getElementById("time").value.toString().toLowerCase();
+
+console.log("Inside of solveKinematics()");
+
+   if( x=="x" && v0!="x" && v!="x" && a!="x" && t!="x") {
+	//Solve for only displacement
+	console.log("Inside of solve only for displacement if statement");
+	var dis = 0.0;
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	dis = vel0 * time + 0.5 * accel * (time * time);
+	return "dis: " + dis + "";
+    }
+    else if( x!="x" && v0=="x" && v!="x" && a!="x" && t!="x") {
+	//Solve for only initial velocity
+	var dis = parseFloat(x);
+	var vel0 = 0.0;
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	vel0 = vel - accel * time;
+	return "vel0: " + vel0 + "";
+   } 
+    else if( x!="x" && v0!="x" && v=="x" && a!="x" && t!="x") {
+	//Solve for only final velocity
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = 0.0;
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	vel = vel0 + accel * time;
+	return "velf: " + vel + "";
+    }
+    else if( x!="x" && v0!="x" && v!="x" && a=="x" && t!="x") {
+	//Solve for only acceleration
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = 0.0;
+	var time = parseFloat(t);
+	accel = (vel - vel0)/time;
+	return "accel: " + accel + "";
+    }
+    else if( x!="x" && v0!="x" && v!="x" && a!="x" && t=="x") {
+	//Solve for only time
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = 0.0;
+	time = (vel - vel0)/accel;
+	return "time: " + time + "";
+    }
+    else if( x=="x" && v0=="x" && v!="x" && a!="x" && t!="x") {
+	//Solve for displacement and initial velocity
+	var dis = 0.0;
+	var vel0 = 0.0;
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	vel0 = vel - accel * time;
+	dis = vel0 * time + 0.5 * accel * (time * time);
+	return "dis: " + dis + " vel0: " + vel0 + "";
+    }
+    else if( x=="x" && v0!="x" && v=="x" && a!="x" && t!="x") {
+	//Solve for displacement and final velocity
+	var dis = 0.0;
+	var vel0 = parseFloat(v0);
+	var vel = 0.0;
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	dis = vel0 * time + 0.5 * accel * (time * time);
+	vel = vel0 + accel * time;
+	return "dis: " + dis + " velf: " + vel + "";
+    }
+    else if( x=="x" && v0!="x" && v!="x" && a=="x" && t!="x") {
+	//Solve for displacement and acceleration
+	var dis = 0.0;
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = 0.0;
+	var time = parseFloat(t);
+	accel = (vel - vel0) / time;
+	dis = vel0 * time + 0.5 * accel * (time * time);
+	return "dis: " + dis + " accel: " + accel + "";
+    }
+    else if( x=="x" && v0!="x" && v!="x" && a!="x" && t=="x") {
+	//Solve for displacement and time
+	var dis = 0.0;
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = 0.0;
+	time = (vel - vel0) / accel;
+	dis = vel0 * time + 0.5 * accel * (time * time);
+	return "dis: " + dis + " time: " + time + "";
+    }
+    else if( x!="x" && v0=="x" && v=="x" && a!="x" && t!="x") {
+	//Solve for initial velocity and final velocity
+	var dis = parseFloat(x);
+	var vel0 = 0.0;
+	var vel = 0.0;
+	var accel = parseFloat(a);
+	var time = parseFloat(t);
+	vel0 = (dis - 0.5 * accel * (time * time))/time;
+	vel = vel0 + accel * time;
+	return "vel0: " + vel0 + " velf: " + vel + "";
+    }
+    else if( x!="x" && v0=="x" && v!="x" && a=="x" && t!="x") {
+	//Solve for initial velocity and acceleration
+	var dis = parseFloat(x);
+	var vel0 = 0.0;
+	var vel = parseFloat(v);
+	var accel = 0.0;
+	var time = parseFloat(t);
+	accel = (dis - vel * time) / (0.5 * (time * time) - (time * time));
+	vel0 = vel - accel * time;
+	return "vel0: " + vel0 + " accel: " + accel + "";
+    }
+    else if( x!="x" && v0=="x" && v!="x" && a!="x" && t=="x") {
+	//Solve for initial velocity and time
+	var dis = parseFloat(x);
+	var vel0 = 0.0;
+	var vel = parseFloat(v);
+	var accel = parseFloat(a);
+	var time = 0.0;
+	vel0 = Math.sqrt((vel * vel) - 2 * accel * dis);
+	time = (vel - vel0)/accel;
+	return "vel0: " + vel0 + " time: " + time + "";
+    }
+    else if( x!="x" && v0!="x" && v=="x" && a=="x" && t!="x") {
+	//Solve for final velocity and acceleration
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = 0.0;
+	var accel = 0.0;
+	var time = parseFloat(t);
+	accel = (dis - vel0 * time) / (2 * (time * time));
+	vel = vel0 + accel * time;
+	return "velf: " + vel + " accel: " + accel + "";
+    }
+    else if( x!="x" && v0!="x" && v=="x" && a!="x" && t=="x") {
+	//Solve for final velocity and time
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = 0.0;
+	var accel = parseFloat(a);
+	var time = 0.0;
+	vel = Math.sqrt((vel0 * vel0) + 2 * accel * dis);
+	time = (vel - vel0) / accel;
+	return "velf: " + vel + " time: " + time + "";
+    }
+    else if( x!="x" && v0!="x" && v!="x" && a=="x" && t=="x") {
+	//Solve for acceleration and time
+	var dis = parseFloat(x);
+	var vel0 = parseFloat(v0);
+	var vel = parseFloat(v);
+	var accel = 0.0;
+	var time = 0.0;
+	accel = ((vel * vel) - (vel0 * vel0)) / (2 * dis);
+	time = (vel - vel0) / accel;
+	return "accel: " + accel + " time: " + time + "";
+    }
 }
 
 function clearAnswerFields() {
